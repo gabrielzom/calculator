5819b8f32cd92500dcb4f00cc42af80adadfde71
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2021-07-13 14:44:31 -0300

    add linear-gradient and animation with css

--- script-calculadora.js
+++ script-calculadora.js
@@ -1,16 +1,19 @@
-let display = document.getElementById('display')
+let display = document.querySelector("#display")
 let formula
 
 SetDisplay = (numberClick) => {
     display.value += numberClick
+    document.querySelector("#display").style.color = "#000000"
 }
 
 Result = () => {
     
     if (Number.isInteger((eval(display.value))) == false) {
         display.value = (eval(display.value)).toFixed(2)
+        document.querySelector("#display").style.color = "#008000"
     } else {
         display.value = (eval(display.value))
+        document.querySelector("#display").style.color = "#008000"
     }
 }
 
@@ -18,10 +21,12 @@ BackSpace = () => {
     formula = display.value
     formula = formula.substring(0, formula.length-1)
     display.value = formula
+    document.querySelector("#display").style.color = "#000000"
 }
 
 Clear = () => {
     display.value = ''
+    document.querySelector("#display").style.color = "#000000"
 }
 
 
